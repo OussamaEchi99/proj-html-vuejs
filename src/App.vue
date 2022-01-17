653b3e3ddd2487017de7bc811e558e09de66bfe9
OussamaEchi99 <oussama.echi99@gmail.com>
2022-01-17 17:32:37 +0100

    Ho termito il header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header />
+    <Header :headerMenuLeft='header_menu_left' :headerMenuRight='header_menu_right' :headerCartSearchMenu='header_menu_cart_search'/>
     <Main />
     <Footer />
   </div>
@@ -18,10 +18,51 @@ export default {
     Main,
     Footer
   },
+  data: function () {
+    return {
+      header_menu_left: [
+        {
+          text: 'Home',
+          img: 'svg-0.svg'
+        },
+        {
+          text: 'Pages'
+        },
+        {
+          text: 'Menu'
+        },
+      ],
+      header_menu_right: [
+        {
+          text: 'Event'
+        },
+        {
+          text: 'Blog'
+        },
+        {
+          text: 'Landing'
+        }
+      ],
+      header_menu_cart_search: [
+        {
+          img: 'svg-1.svg',
+          text: 'Cart',
+          value: true
+        },
+        {
+          img: 'search-solid.svg',
+          text: 'Search',
+          value: false
+        }
+      ]
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 @import './style/General.scss';
+@import './style/Common.scss';
+@import './style/Variables.scss';
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,131 @@
 <template>
     <header>
-        <div>
-
+        <div class="container">
+            <div class="order_btn">
+                <a class="primary_btn" href="#">ORDER ONLINE</a>
+            </div>
+            <div class="menu_logo">
+                <ul>
+                    <li v-for="(textMenu, index) in headerMenuLeft" :key="index">
+                        <a href="#">
+                            <img v-if="textMenu.img" :src="require(`../assets/img/${textMenu.img}`)" :alt="textMenu.text">
+                            {{ textMenu.text }}
+                        </a>
+                    </li>
+                    <li class="logo"><img src="../assets/img/h5-logo-divided-header.png" alt="Logo Image"></li>
+                    <li v-for="(textMenu, index) in headerMenuRight" :key="index">
+                        <a href="#">
+                            {{ textMenu.text }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="cart_search_menu">
+                <ul>
+                    <li v-for="(textMenuCartSearch, index) in headerCartSearchMenu" :key="index">
+                        <a href="#">
+                            <img class="svg_invert" :src="require(`../assets/img/${textMenuCartSearch.img}`)" alt="">
+                            {{ textMenuCartSearch.text }}
+                            <span v-if="textMenuCartSearch.value" class="cart_number">0</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </header>
 </template>
 
 <script>
 export default {
-    
+    name: 'Header',
+    props: {
+        headerMenuLeft: Array,
+        headerMenuRight: Array,
+        headerCartSearchMenu: Array
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../style/Variables.scss';
+
     header{
-        height: 100px;
+        height: 125px;
         background-image: url('../assets/img/sky.png');
+        font-size: 12px;
+        color: white;
+
+        a{
+            text-transform: uppercase;
+            font-weight: bold;
+        }
+
+        .container{
+            height: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            .order_btn{
+                a{
+                    padding: 15px 30px;
+                }
+            }
+            
+            .menu_logo{
+                ul{
+                    display: flex;
+                    align-items: center;
+
+                    .logo{
+                        width: 140px;
+                    }
+
+                    a{
+                        margin: 0 20px;
+                        display: flex;
+
+                        img{
+                            filter: invert(1);
+                            width: 16px;
+                            margin-right: 5px;
+                        }
+                    }
+                }
+            }
+
+            .cart_search_menu{
+                ul{
+                    display: flex;
+                    align-items: center;
+                    li{
+                        display: flex;
+                        align-items: center;
+                        margin: 0 10px;
+                        a{
+                            display: flex;
+                            align-items: center;
+                            position: relative;
+
+                            .cart_number{
+                                position: absolute;
+                                top: -7px;
+                                left: -6px;
+                                font-size: 7px;
+                                padding: 3px 6px;
+                                background-color: white;
+                                border-radius: 50%;
+                                color: $brand_primary_color;
+                            }
+                        }
+                        img{
+                            width: 20px;
+                            margin-right: 5px;
+                        }
+                    
+                    }
+                }
+            }
+        }
     }
 </style>
