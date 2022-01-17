8d61245adc5b8511a6b7a0ffa29e0dcdfc56a0ac
OussamaEchi99 <oussama.echi99@gmail.com>
2022-01-17 12:17:20 +0100

    Struttura

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div>
+
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    header{
+        height: 100px;
+        background-image: url('../assets/img/sky.png');
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <main>
+        <div></div>
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    // main{
+    //     height: 300px;
+    //     background-image: url('../assets/img/sky.png');
+    // }
+</style>
