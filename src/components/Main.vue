7ffedd901c4f86ce5f481f3760bcae0f022da8c5
OussamaEchi99 <oussama.echi99@gmail.com>
2022-01-18 13:26:07 +0100

    Aggiunto e completato la sezione Specials

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,6 +3,7 @@
         <Banner />
         <Events :eventsImages="events" />
         <Proverbs />
+        <Specials :specialsMenuDetails="specialsMenu"/>
     </main>
 </template>
 
@@ -10,13 +11,15 @@
 import Banner from "./Banner.vue";
 import Events from "./Events.vue";
 import Proverbs from "./Proverbs.vue";
+import Specials from "./Specials.vue";
 
 export default {
     name: 'Main',
     components: {
         Banner,
         Events,
-        Proverbs
+        Proverbs,
+        Specials
     },
     data: function () {
         return {
@@ -32,6 +35,23 @@ export default {
                 },
                 {
                     img: '4'
+                }
+            ],
+            specialsMenu: [
+                {
+                    title: 'Combo Piccolo',
+                    cost: 10,
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut aliquam dui'
+                },
+                {
+                    title: 'Combo Mezzo',
+                    cost: 20,
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut aliquam dui'
+                },
+                {
+                    title: 'Combo Grande',
+                    cost: 30,
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut aliquam dui'
                 }
             ]
         }
---
+++ src/components/Specials.vue
@@ -0,0 +1,97 @@
+<template>
+    <section>
+        <div class="col">
+            <img src="../assets/img/h1-img-4.jpg" alt="Pizza Specials Image">
+            <div class="pizza_specials_logo">
+                <img src="../assets/img/h1-img-7n.png" alt="Pizza Specials Logo">
+            </div>
+        </div>
+        <div class="col pizzas_specials_products">
+            <div class="pizza_specials_menu">
+                <h2>SPECIALS&#42;</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br> Praesent ut.</p>
+                <ul>
+                    <li v-for="(menuDetail, index) in specialsMenuDetails" :key="index" class="menu">
+                        <span>${{ menuDetail.cost }}</span>
+                        <ul class="description">
+                            <li>
+                                <span>{{ menuDetail.title }}</span>
+                                <p>{{ menuDetail.description }}</p>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <a href="#" class="pizza_svg pizza_svg_grey"><img src="../assets/img/svg-4.svg" alt="Pizza Image"></a>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Specials',
+    props: {
+        specialsMenuDetails: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/Variables.scss';
+
+    section{
+        display: flex;
+        .col{
+            width: calc(100% / 2);
+            margin: 5px 0;
+            position: relative;
+
+            .pizza_specials_logo{
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+            }
+        }
+        
+        .pizzas_specials_products{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+
+            .pizza_specials_menu{
+                width: 42%;
+                margin: auto;
+
+                h2{
+                    font-size: 40px;
+                }
+                .menu{
+                    display: flex;
+                    margin: 17px 0;
+                    > span{
+                        color: $brand_primary_color;
+                        font-weight: bold;
+                        font-size: 26px;
+                    }
+                    .description{
+                        margin-left: 15px;
+                        span{
+                            text-transform: uppercase;
+                            font-weight: bold;
+                            font-size: 26px;
+                        }
+                        p{
+                            font-size: 13px;
+                        }
+                    }
+                }
+            }
+
+            .pizza_svg{
+                bottom: 175px;
+            }
+        }
+    }
+</style>
